7d8546d71637144fc9c769138d4fbc5d34dd14ea
Isaac Carnes <isaacjcarnes@gmail.com>
2023-05-07 21:45:12 -0400

    Added labels to inputs and small css improvements

--- src/js/Header.js
+++ src/js/Header.js
@@ -3,9 +3,14 @@ function Header({width, setWidth, height, setHeight}) {
 
   return (
     <div id="Header">
+      <label>
+        Width: 
       <input type="number" value={width} onChange={(e) => {e.preventDefault(); if(true){setWidth(e.target.value)}}}/>
+      </label>
+      <label>
+        Height:
       <input type="number" value={height} onChange={(e) => {e.preventDefault(); if(true){setHeight(e.target.value)}}}/>
-
+      </label>
         <h1>The Lost Royal</h1>
     </div>
   );
